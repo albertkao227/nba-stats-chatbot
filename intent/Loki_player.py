c82dfa1509416f66ccabfd5282c2e45333b133d1
Albert <albertkao227@gmail.com>
2021-09-16 23:50:26 -0700

    Update utterance

--- intent/Loki_player.py
+++ intent/Loki_player.py
@@ -21,6 +21,45 @@ DEBUG_player = True
 with open("./intent/nba_teams.json", encoding="utf-8") as f:
     teamDICT = json.loads(f.read())
 
+statDICT = {'得分':[], 
+            '籃板':['搶籃板'], '助攻':[], 
+            '阻攻':['火鍋', '蓋火鍋','搧帽', '蓋帽'], 
+            '抄截':[]}
+
+aggDICT = {'平均':['場均'], '單場':['一場']}
+
+levelDICT = {'平均':['場均'], '單場':['一場']}
+
+
+def get_team(resultDICT, args):
+    """Get NBA team name"""
+    print(args)
+    for i in range(len(args)):
+        for key in teamDICT.keys():
+            if args[i] in teamDICT[key]:
+                resultDICT['team'].append(key)
+    return resultDICT
+
+
+def get_stat(resultDICT, args):
+    """Get target stats"""
+    print(args)
+    for i in range(len(args)):
+        for key in statDICT.keys():
+            if args[i] in statDICT[key]:
+                resultDICT['stat'].append(key)
+    return resultDICT
+
+
+def get_level(resultDICT, args):
+    """Get stats aggregation level"""
+    print(args)
+    for i in range(len(args)):
+        for key in levelDICT.keys():
+            if args[i] in levelDICT[key]:
+                resultDICT['level'].append(key)
+    return resultDICT
+
 
 def get_team(resultDICT, args):
     """Get NBA team name"""
@@ -40,20 +79,62 @@ def debugInfo(inputSTR, utterance):
 
 def getResult(inputSTR, utterance, args, resultDICT):
     debugInfo(inputSTR, utterance)
-    resultDICT["team"] = []
+    resultDICT['team'] = []
+    resultDICT['stat'] = []
+    resultDICT['level'] = []
     if utterance == "[公牛][主力]是誰":
         resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
 
     if utterance == "[公牛]誰是[主力]":
         resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
 
     if utterance == "誰是[公牛][最強]的球員":
         resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
 
     if utterance == "誰是[公牛]的[主力]":
         resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
 
     if utterance == "誰是[公牛]籃球[最強]的球員":
         resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
+
+    if utterance == "[2019][公牛隊][平均][得分][最多]的球員":
+        resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
+
+    if utterance == "[2019][公牛隊][平均][得分]超過[30]的球員":
+        resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
+
+    if utterance == "[2019][球季][公牛隊][平均][得分][最多]的球員":
+        resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
+
+    if utterance == "[2019][球季][公牛隊][平均][得分]超過[30]的球員":
+        resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
+
+    if utterance == "[2019年][公牛隊][平均][得分][最多]的球員":
+        resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
+
+    if utterance == "[2019年][公牛隊][平均][得分]超過[30]的球員":
+        resultDICT = get_team(resultDICT, args)
+        resultDICT = get_stat(resultDICT, args)
+        resultDICT = get_level(resultDICT, args)
 
     return resultDICT
