85a23d8c89f5b1cb5da17fce94bb177615729551
Albert <albertkao227@gmail.com>
2021-09-19 17:00:29 -0700

    Udpate

--- infobot.py
+++ infobot.py
@@ -12,6 +12,24 @@ with open("account.info", encoding="utf-8") as f:
     accountDICT = json.loads(f.read())
 
 
+info_fields = {'season': None,
+                'team': None,
+                'player': None,
+                'stat': None,
+                'level': None} 
+
+statDICT = {'pts':['得分'], 
+            'reb':['籃板', '搶籃板'], 'ast':['助攻'], 
+            'blk':['阻攻', '火鍋', '蓋火鍋','搧帽', '蓋帽'], 
+            'stl':['抄截']}
+
+seasonDICT = {'2016':['2016', '2016球季', '2016賽季', '二零一六球季', '二零一六賽季'], 
+              '2017':['2017', '2017球季', '2017賽季', '二零一七球季', '二零一七賽季'],
+              '2018':['2018', '2018球季', '2018賽季', '二零一八球季', '二零一八賽季'],
+              '2019':['2019', '2019球季', '2019賽季', '二零一九球季', '二零一九賽季'],
+              '2020':['2020', '2020球季', '2020賽季', '二零二零球季', '二零二零賽季']}
+
+
 class BotClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {} with id {}'.format(self.user, self.user.id))
@@ -33,32 +51,50 @@ class BotClient(discord.Client):
             elif msg.strip() == 'ping ping':
                 await message.reply('pong pong')
             else:
-
-                filterLIST = []
-                resultDICT = runLoki([msg], filterLIST)        
+                
                 df = pd.read_csv('./test_data.csv')
 
-                team = resultDICT['team'][0]
-                stat = resultDICT['stat'][0]
-                level = resultDICT['level'][0]
+                for name in list(set(df['player_name'])):
+                    if name in msg:
+                        info_fields['player'] = name
+                        msg = msg.replace(name, '').strip()
+                        
+                for season_key in seasonDICT.keys():
+                    for year in seasonDICT[season_key]:
+                        if year in msg:
+                            info_fields['season'] = season_key
+                            msg = msg.replace(year, '').strip()
 
-                if level == '平均':
-                    qry = DataPuller.get_agg_stats(stat, 10, team, 2020)
-                elif level == '單場':
-                    qry = DataPuller.get_single_game_stats(stat, 10, team, 2020)
+                filterLIST = []
+                resultDICT = runLoki([msg], filterLIST)   
+                
+                if len(resultDICT['team']) > 0:
+                    info_fields['team'] = resultDICT['team'][0]
+                
+                info_fields['stat'] = resultDICT['stat'][0]
+                info_fields['level'] = resultDICT['level']
+
+                if info_fields['player'] and info_fields['level'] == 'avg':
+                    qry = DataPuller.get_player_agg_stats(info_fields['stat'], 
+                                                          info_fields['player'], 
+                                                          info_fields['season'])
+
+                elif info_fields['player'] and info_fields['level'] == 'single':
+                    qry = DataPuller.get_player_single_game_stats(info_fields['stat'], 
+                                                                  info_fields['player'], 
+                                                                  info_fields['season'])
+                elif info_fields['team']:
+                    qry = DataPuller.get_team_best_player_stats(info_fields['stat'], 
+                                                                info_fields['team'], 
+                                                                info_fields['season'])
+
                 else:
                     raise ValueError('invalid input') 
 
-                statDICT = {'pts':['得分'], 
-                            'reb':['籃板', '搶籃板'], 'ast':['助攻'], 
-                            'blk':['阻攻', '火鍋', '蓋火鍋','搧帽', '蓋帽'], 
-                            'stl':['抄截']}
 
                 result = ps.sqldf(qry, locals())
-                reply_message = level + statDICT[stat][0] + '最高的是' \
-                                + result['player_name'].iloc[0] + ': ' + str(round(result['target_stat'].iloc[0], 2))
 
-                await message.reply(reply_message)
+                await message.reply(result)
                 
         
 
